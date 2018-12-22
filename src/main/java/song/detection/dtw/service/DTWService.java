7370c17473192c31b2423dd5feaee308cc35546c
Priit Paluoja <priitpaluoja@gmail.com>
2018-12-22 10:55:05 +0200

    Show song titles as result

--- src/main/java/song/detection/dtw/service/DTWService.java
+++ src/main/java/song/detection/dtw/service/DTWService.java
@@ -2,12 +2,21 @@ package song.detection.dtw.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.mp3.Mp3Parser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+import song.detection.dtw.dto.ResultDTO;
 import song.detection.dtw.dto.UploadResultsDTO;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,8 +28,7 @@ public class DTWService {
 
     public UploadResultsDTO analyzeTracks(List<MultipartFile> uploads) {
         List<byte[]> files = uploads.stream().map(this::uploadToBytes).collect(Collectors.toList());
-        // TODO: implement
-        return new UploadResultsDTO();
+        return new UploadResultsDTO(files.stream().map(this::convertToDTO).collect(Collectors.toList()));
     }
 
 
@@ -33,4 +41,19 @@ public class DTWService {
         }
     }
 
+    // https://stackoverflow.com/questions/1645803/how-to-read-mp3-file-tags
+    private ResultDTO convertToDTO(byte[] song) {
+        try (InputStream input = new ByteArrayInputStream(song)) {
+
+            Metadata metadata = new Metadata();
+
+            new Mp3Parser().parse(input, new DefaultHandler(), metadata, new ParseContext());
+
+            // TODO: error handling if title not present?
+            return new ResultDTO(metadata.get("title"));
+
+        } catch (IOException | TikaException | SAXException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
