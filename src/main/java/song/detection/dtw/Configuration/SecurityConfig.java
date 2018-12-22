c635215946848f799ab7f555339a0c4b8eb917e6
Priit Paluoja <priitpaluoja@gmail.com>
2018-12-22 09:25:42 +0200

    Setup initial Spring Boot

---
+++ src/main/java/song/detection/dtw/Configuration/SecurityConfig.java
@@ -0,0 +1,31 @@
+package song.detection.dtw.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .permitAll();
+
+        http.authorizeRequests()
+                .antMatchers("/*")
+                .permitAll();
+
+        http.authorizeRequests()
+                .anyRequest()
+                .fullyAuthenticated();
+
+
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/?logoutsuccess");
+    }
+}
---
+++ src/main/java/song/detection/dtw/DtwApplication.java
@@ -0,0 +1,14 @@
+package song.detection.dtw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtwApplication.class, args);
+	}
+
+}
+
